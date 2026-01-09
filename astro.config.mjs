bf9f2f6a2a034dc47eea41d36c5c54a2c85383d3
Jordy756 <jordycastro1756@gmail.com>
2026-01-08 19:38:38 -0600

    feat(4G-001): update avatar image URL in English CV feat(4G-002): update avatar image URL in Spanish CV style(4G-003): import font using Font component in layout style(4G-004): adjust spacing in Hero section layout style(4G-005): update image height and max height in Hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
@@ -7,7 +7,9 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://jordy756-minimalist-portfolio.vercel.app",
+
 	adapter: vercel(),
+
 	i18n: {
 		defaultLocale: "en",
 		locales: ["es", "en"],
@@ -18,6 +20,23 @@ export default defineConfig({
 			fallbackType: "rewrite"
 		}
 	},
+
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: "Nunito Sans",
+				cssVariable: "--font-nunito-sans",
+				weights: [400, 600, 700, 800],
+				styles: ["normal"],
+				subsets: ["latin"],
+				display: "swap",
+				fallbacks: ["system-ui", "sans-serif"],
+				unicodeRange: ["U+0020-007F", "U+00A0-017F"]
+			}
+		]
+	},
+
 	vite: {
 		plugins: [tailwindcss()]
 	}
